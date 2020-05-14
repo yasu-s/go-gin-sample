f563c2cd62540f3a14ad153d03bb3cac2144dd34
Yasuyuki Saito <yasuyuki.saito.2012@gmail.com>
2020-05-14 23:48:53 +0000

    swag設定

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
+// @title Sample API
+// @version 1.0
+// @description APIサンプルです
+// @host localhost:8080
+// @BasePath /api/v1
+
+// API test
+// @Summary API test
+// @Description APIサンプルです
+// @Accept  json
+// @Produce  json
+// @Router /ping [get]
 func main() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
